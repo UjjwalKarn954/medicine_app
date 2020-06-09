b9f36e16bc658e6988402243b74fb263e5785d0b
unknown <ujjwalkarn954@gmail.com>
2020-06-09 10:38:52 +0530

    Ujjwal_update_0906

--- src/view/LoginUserInterface.java
+++ src/view/LoginUserInterface.java
@@ -17,17 +17,24 @@ public class LoginUserInterface {
 	
 		Pane p = new Pane();
 		Label lbl = new Label("Welcom To JU Page");
+		Label lb2 = new Label("User_ID");
+		Label lb3 = new Label("Password");
 		TextField text1=new TextField();
 		TextField text2=new TextField();
 		Button but1=new Button("Login");
 		Button but2=new Button("Register");
 		text1.setPromptText("User_id");
-		setupTextUI(text1, "Calibri",20 , 400,Pos.BASELINE_LEFT , 100, 100);
-		setupTextUI(text2, "Calibri",20 , 400,Pos.BASELINE_LEFT , 100, 150);
+		text2.setPromptText("password");
+		setupTextUI(text1, "Calibri",20 , 400,Pos.BASELINE_LEFT , 140, 100);
+		setupTextUI(text2, "Calibri",20 , 400,Pos.BASELINE_LEFT , 140, 150);
 		setupLabelUI(lbl, "Calibri", 20, 50, Pos.BASELINE_CENTER, 50, 50, "BLUE");
+		setupLabelUI(lb2, "Calibri", 20, 50, Pos.BASELINE_CENTER, 50, 100, "BLUE");
+		setupLabelUI(lb3, "Calibri", 20, 50, Pos.BASELINE_CENTER, 50, 150, "BLUE");
 		setupButtonUI(but1, "Calibri", 20, 50, Pos.BASELINE_CENTER, 150, 200);
 		setupButtonUI(but2, "Calibri", 20, 50, Pos.BASELINE_CENTER, 150, 250);
 		p.getChildren().addAll(lbl);
+		p.getChildren().addAll(lb2);
+		p.getChildren().addAll(lb3);
 		p.getChildren().addAll(text1);
 		p.getChildren().addAll(text2);
 		p.getChildren().addAll(but1);
@@ -42,31 +49,52 @@ public class LoginUserInterface {
 	} 
 	public void constructRegisterUI(Stage theStage) {
 		Pane p = new Pane();
-		Label lbl = new Label("This is the register page");
-		setupLabelUI(lbl, "Algerian", 72, 100, Pos.BASELINE_CENTER, 200, 250, "RED");
-		p.getChildren().add(lbl);
+		Label lbl = new Label("Welcom To JU Page");
+		Label lb2 = new Label("Name");
+		Label lb3 = new Label("Company_Name");
+		Label lb4 = new Label("Email_id");
+		Label lb5 = new Label("Pasword");
+		Label lb6 = new Label("Con_Password");
+		TextField text1=new TextField();
+		TextField text2=new TextField();
+		TextField text3=new TextField();
+		TextField text4=new TextField();
+		TextField text5=new TextField()
+		Button but1=new Button("Register");
+		text1.setPromptText("Name");
+		text2.setPromptText("Company-name");
+		text3.setPromptText("Email_id");
+		text4.setPromptText("password");
+		text5.setPromptText("Con_password");
+		setupTextUI(text1, "Calibri",20 , 400,Pos.BASELINE_LEFT , 140, 100);
+		setupTextUI(text2, "Calibri",20 , 400,Pos.BASELINE_LEFT , 140, 150);
+		setupTextUI(text3, "Calibri",20 , 400,Pos.BASELINE_LEFT , 140, 200);
+		setupTextUI(text4, "Calibri",20 , 400,Pos.BASELINE_LEFT , 140, 250);
+		setupTextUI(text5, "Calibri",20 , 400,Pos.BASELINE_LEFT , 140, 300);
+		setupLabelUI(lbl, "Calibri", 20, 50, Pos.BASELINE_CENTER, 50, 50, "BLUE");
+		setupLabelUI(lb2, "Calibri", 20, 50, Pos.BASELINE_CENTER, 50, 100, "BLUE");
+		setupLabelUI(lb3, "Calibri", 20, 50, Pos.BASELINE_CENTER, 50, 150, "BLUE");
+		setupLabelUI(lb4, "Calibri", 20, 50, Pos.BASELINE_CENTER, 50, 200, "BLUE");
+		setupLabelUI(lb5, "Calibri", 20, 50, Pos.BASELINE_CENTER, 50, 250, "BLUE");
+		setupLabelUI(lb6, "Calibri", 20, 50, Pos.BASELINE_CENTER, 50, 300, "BLUE");
+		setupButtonUI(but1, "Calibri", 20, 50, Pos.BASELINE_CENTER, 150, 500);
+		p.getChildren().addAll(lbl);
+		p.getChildren().addAll(lb2);
+		p.getChildren().addAll(lb3);
+		p.getChildren().addAll(lb4);
+		p.getChildren().addAll(lb5);
+		p.getChildren().addAll(lb6);
+		p.getChildren().addAll(text1);
+		p.getChildren().addAll(text2);
+		p.getChildren().addAll(text3);
+		p.getChildren().addAll(text4);
+		p.getChildren().addAll(text5);
+		p.getChildren().addAll(but1);
 		Scene scn = new Scene(p,1200,1200);
 		theStage.setTitle("Register Page");
 		theStage.setScene(scn);
+	
 	}
-	// public Pane constructRrgisterUI(Pane p1) {
-	// 	Label lbl = new Label("Welcom To JU Page");
-	// 	TextField text1=new TextField("Text_1");
-	// 	TextField text2=new TextField("Text_2");
-	// 	Button but1=new Button("Login");
-	// 	Button but2=new Button("Register");
-	// 	setupTextUI(text1, "Calibri",20 , 30,Pos.BASELINE_LEFT , 100, 100);
-	// 	setupTextUI(text2, "Calibri",20 , 30,Pos.BASELINE_LEFT , 100, 150);
-	// 	setupLabelUI(lbl, "Calibri", 20, 50, Pos.BASELINE_CENTER, 50, 50, "BLUE");
-	// 	setupButtonUI(but1, "Calibri", 20, 50, Pos.BASELINE_CENTER, 150, 200);
-	// 	setupButtonUI(but2, "Calibri", 20, 50, Pos.BASELINE_CENTER, 150, 250);
-	// 	p1.getChildren().addAll(lbl);
-	// 	p1.getChildren().addAll(text1);
-	// 	p1.getChildren().addAll(text2);
-	// 	p1.getChildren().addAll(but1);
-	// 	p1.getChildren().addAll(but2);
-    //     return p1;
-	// } 
 
     public void setupButtonUI(Button b, String ff, double f, double w, Pos p, double x, double y){
 		b.setFont(Font.font(ff, f));
